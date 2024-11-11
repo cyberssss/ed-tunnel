fd3f16a20265143a923a849f89d92aa65448d039
Cyberpunk_Rajesh_3 <106486165+cyberssss@users.noreply.github.com>
2024-11-11 11:13:26 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '1d52d9ee-f05c-4a95-b1e9-2583c0d90be1';
 
-const พร็อกซีไอพีs = ['165.232.182.81'];
+const พร็อกซีไอพีs = ['138.2.94.123'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
